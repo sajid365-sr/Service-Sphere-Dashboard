6fdce61255f6fe11ff4d53be9813b3d9a63f66e8
sajid365-sr <sajidsorker86@gmail.com>
2024-08-29 02:38:21 +0600

    fixing comment

--- app/(dashboard)/[storeid]/(routes)/collections/components/collection-columns.jsx
+++ app/(dashboard)/[storeid]/(routes)/collections/components/collection-columns.jsx
@@ -3,8 +3,6 @@
 import Image from "next/image";
 import CellAction from "./cell-action";
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
 export const collectionColumns = [
   {
     accessorKey: "name",
